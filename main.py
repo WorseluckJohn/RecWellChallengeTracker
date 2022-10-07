9868d718b093b968c963593b42db0db237033c84
Ahmet Oguz <84751789+WorseluckJohn@users.noreply.github.com>
2022-10-07 18:21:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import summary
 import tracking
 
 if __name__ == '__main__':
-    keyName = "challenge-tracking-python-ca37993cfad5.json"
+    keyName = "YOURKEYFILENAME.json"
     masterSheetName = "Master Challenges Sheet Original"
     
     tracking.main(keyName, masterSheetName)
