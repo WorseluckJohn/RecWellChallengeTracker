130e11ea95c179f4fb77995a5154ec27ecc8a571
Ahmet Oguz <84751789+WorseluckJohn@users.noreply.github.com>
2022-10-31 15:27:55 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import summary
-import tracking
-
-if __name__ == '__main__':
-    keyName = "YOURKEYFILENAME.json"
-    masterSheetName = "Master Challenges Sheet Original"
-    
-    tracking.main(keyName, masterSheetName)
-    summary.main(keyName, masterSheetName)
